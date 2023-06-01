cdf989e3303f88034a38d8f03aeff0ffa9e11301
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-05-31 18:10:39 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,3 @@
 #!/usr/bin/env ruby
-
-regex = /\bSchool\b/
-string = ARGV[0]
-
-match = regex.match(string)
-
-puts match ? match[0] : ""
-puts "(#{match ? match[0].length : 0} chars long)"
+#Auth: Sangwani P Zyambo
+puts ARGV[0].scan(/School/).join
